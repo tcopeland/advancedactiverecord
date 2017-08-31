fa3746f09979f27181fd181c0f5dc6eb4cb21692
Tom Copeland <tom@motorefi.com>
2017-08-31 16:21:29 -0400

    Faker

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'pg'
 gem 'bootsnap'
 gem 'aasm'
+gem 'faker'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-reviewer_sally = Reviewer.create!(name: "Sally Sallysdaughter")
-reviewer_fred = Reviewer.create!(name: "Fred Fredson")
+reviewer1 = Reviewer.create!(name: Faker::Name.name)
+reviewer2 = Reviewer.create!(name: Faker::Name.name)
 
 brothers_k = Book.create!(title: "The Brothers Karamazov")
 fyodor_d = Author.create!(name: "Fyodor Dostoevsky")
 brothers_k.authors << fyodor_d
-Review.create!(book: brothers_k, reviewer: reviewer_fred, content: "a classic").tap {|r| r.publish! }
-Review.create!(book: brothers_k, reviewer: reviewer_sally, content: "great", featured: true).tap {|r| r.publish! }
+Review.create!(book: brothers_k, reviewer: reviewer2, content: "a classic").tap {|r| r.publish! }
+Review.create!(book: brothers_k, reviewer: reviewer1, content: "great", featured: true).tap {|r| r.publish! }
 
 fatal_e = Book.create!(title: "The Fatal Eggs")
 mikhail_b = Author.create!(name: "Mikhail Bulgakov")
