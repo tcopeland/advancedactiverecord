e240b2a4a88ff7d8c4d01f282ee2ca12c4f32ace
Tom Copeland <tom@motorefi.com>
2017-08-31 14:50:13 -0400

    More odds and ends

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,11 @@
 class Book < ApplicationRecord
   has_and_belongs_to_many :authors
 
-  has_many :reviews
+  has_many :reviews do
+    def featured
+      where(featured: true).first
+    end
+  end
+  
+  has_one :featured_review, -> { featured }, class_name: 'Review'
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,10 @@ class Review < ApplicationRecord
 
   belongs_to :book
   belongs_to :reviewer
+  
+  scope :featured, -> { where(featured: true) }
+  
+  validates :featured, uniqueness: {scope: :book_id, message: "review already exists for this book"}, if: Proc.new {|r| r.featured? }
 
   include AASM
 
